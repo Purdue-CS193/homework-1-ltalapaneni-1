7e4593f742faab7aaad284ee82863df0e7dfd06c
Lakshmi Talapaneni <ltalapan@data.cs.purdue.edu>
2021-09-15 11:37:10 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -3,43 +3,45 @@
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
-
+# Task 1
+mv Homework/MA165/HW1.txt Homework/MA165.HW2.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/StarWars/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp ~/Programs/Java ~/Homework/CS180/Project1
 
 # Task 8
-
+mv Programs/Count.c && Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py && Programs/Python3
 
 # Task 10
-
+touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+ls -al
 
 # Task 12
 : '
 Type your vim/nano customizations below:
 
-
-
 '
+~/.vimrc :set number
+~/.vimrc :colorscheme onedark
+~/.vimrc :set autoindent expandtab
+
